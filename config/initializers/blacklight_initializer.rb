a16746a1554cee6b0051b1cb855be9966243e0ff
Paul Mather <pmather@vt.edu>
2015-10-26 15:32:39 -0400

    Initial Sufia application
    This is the code for a vanilla Sufia application that results from
    following the install instructions at
    https://github.com/projecthydra/sufia.
    
    Basically, this is having done a "rails new" followed by adding the
    "sufia" and "kaminari" gems and executing "rails generate sufia:install
    -f" to add the Sufia code to the Rails application.

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'd8a207834a9660d3294bcf9427f214972001e4f9d5b9434b29ed3a06aacecd4db8b7ecb92aacdaf595ac7e0c40f6c51f9b5c60a3e673d8c0c98d18fe0e266fc4'
+
