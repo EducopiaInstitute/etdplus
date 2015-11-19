61444b6105d4dc5d83f5a5f3d7c9ca52f0b6be1f
Collin Brittle <cnbrittle@gmail.com>
2015-11-19 17:58:05 +0000

    Use the standard Rails configuration, rather than Sufia's.
    The configuration options are now under Rails.configuration.x.*

--- config/initializers/etdplus.rb
+++ config/initializers/etdplus.rb
@@ -1,24 +1,25 @@
-EtdPlus.config do |config|
-  # This is a dictionary with tools as keys, and metadata entries as items in a list. The items
-  # added will not be displayed in the user interface.
-  config.fits_display_blacklist = {
-    #'Jhove': ['size', 'well-formed'],
-    #'Exiftool': ['creatingApplicationName'],
-    #'NLNZ Metadata Extractor': ['hasForms'],
-  }
+# This is a dictionary with tools as keys, and metadata entries as items in a list. The items
+# added will not be displayed in the user interface. An asterisk ('*') means that NO ITEMS from
+# that tool will be displayed.
+Etdplus::Application.config.x.fits_display_blacklist = {
+  #'Jhove': ['size', 'well-formed'],
+  #'Exiftool': ['creatingApplicationName'],
+  #'NLNZ Metadata Extractor': ['*'],
+}
 
-  # This is a dictionary with tools as keys, and metadata entries as items in a list. The items
-  # added will be exported with the METS metadata in any Bagit bag.
-  config.fits_export_whitelist = {
-    #'Jhove': ['valid', 'version'],
-    #'OIS File Information': ['md5checksum']
-  }
+# This is a dictionary with tools as keys, and metadata entries as items in a list. The items
+# added will be exported with the METS metadata in any Bagit bag. An asterisk ('*') means that
+# ALL ITEMS from that tool will be exported.
+Etdplus::Application.config.x.fits_export_whitelist = {
+  #'Jhove': ['valid', 'version'],
+  #'OIS File Information': ['md5checksum']
+  #'Droid': ['*']
+}
 
-  # If a virus is detected in a file, by default the file will be deleted. If this is false, the
-  # file will be forced to a private visibility.
-  config.destroy_viruses_immediately = True
+# If a virus is detected in a file, by default the file will be deleted. If this is false, the
+# file will be forced to a private visibility.
+Etdplus::Application.config.x.destroy_viruses_immediately = true
 
-  # If PII is detected in a file, by default the file will be deleted. If this is false, the
-  # file will be forced to a private visibility.
-  config.destroy_pii_immediately = True
-end
+# If PII is detected in a file, by default the file will be deleted. If this is false, the
+# file will be forced to a private visibility.
+Etdplus::Application.config.x.destroy_pii_immediately = true
