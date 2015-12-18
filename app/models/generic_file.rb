4d9e990e2f60ea8d4df04c1afdd59dde712d6c0f
Tingting Jiang <tvj@vt.edu>
2015-12-18 20:47:15 +0000

    Detect PII info and send notification email

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -1,63 +1,43 @@
-require 'fileutils'
-
 class GenericFile < ActiveFedora::Base
   include Sufia::GenericFile
-  include Etdplus::GenericFile::PiiDetect
 
-  contains 'piiMetadata', class_name: 'PiiDatastream'
+  before_save :bulk_extractor_inspect
 
-  property :ssn, delegate_to: 'piiMetadata' do |index|
-    index.as :stored_searchable
-  end
-  property :ccn, delegate_to: 'piiMetadata' do |index|
-    index.as :stored_searchable
-  end
+  property :pii_inspect_event, predicate: ::RDF::DC.provenance
 
-  property :virus_scan_result, predicate: ::RDF::DC.provenance do |index|
-    index.as :stored_searchable, :facetable
-  end
+  private	
+        def bulk_extractor_inspect
+          filename = Rails.root.join("tmp", "#{self.id}_text_content.txt")
+          text_file = File.new(filename, 'w')
+          text_file.write append_metadata.gsub("\n", " ")
+          text_file.close
+          filename = text_file.path
+          cmd_string = "bulk_extractor -o tmp/bulk_extractor_output #{filename}"
+          Open3.popen3(cmd_string)
+          sleep(1)
+          found_pii = []
+          Dir.chdir("tmp/bulk_extractor_output") do
+            found_pii = Dir.foreach('.').select { |entry|
+              (entry == "ccn.txt" || entry == "pii.txt") && File.size(entry) > 0
+            }
+          end
+          FileUtils.rm_rf("tmp/bulk_extractor_output") # output dir must not exist
+          File.delete(filename)
+          depositor = User.find_by_user_key(self.depositor)
+          gf_actor = Sufia::GenericFile::Actor.new(self, depositor)
+          if found_pii.empty?
+            gf_actor.update_metadata({pii_inspect_event: ['No pii detected']}, visibility)
+            return true
+          elsif found_pii.length == 2
+            message = 'Found SSN and Credit Card Number'
+          else
+            message = (found_pii[0] == "pii.txt" ? "Found SSN" : "Found Credit Card Number")
+          end
+          logger.warn(message)
+          errors.add(:base, message)
+          PiiMailer.destroy_file(id).deliver_later
+          gf_actor.destroy
+          return false
+        end
 
-  property :pii_scan_result, predicate: ::RDF::DC.provenance do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  def bulk_extractor_inspect
-    #filename = File.join(File.dirname(__FILE__), '..', '..', 'tmp', 'gf_text_content', "#{self.id}_text_content.txt")
-    #text_file = File.new(filename, 'w')
-    #self.characterize
-    #text_file.write append_metadata.gsub("\n", "")
-    #text_file.close
-    config = YAML.load_file(Rails.root.join('config', 'fedora.yml'))[ENV["RAILS_ENV"]]
-    uri = self.load_attached_files[0]
-    text_file = open(uri, :http_basic_authentication => [config['user'], config['password']])
-    filename = text_file.path
-    #text_file.close
-    Dir.chdir("/Users/virjtt03/bulk_extractor-1.5.5")
-    cmd_string = "bulk_extractor -o output2 #{filename}"
-    #puts cmd_string
-    Open3.popen3(cmd_string)
-    #File.open(text_file, 'r') do |file|
-      #file.each_line {|line| puts line }
-    #end
-    sleep(1)
-    Dir.chdir("./output2")
-    found_pii = Dir.foreach('.').select { |entry|
-      #if File.file?(entry) && File.readable?(entry) && (entry == "pii.txt" || entry == "ccn.txt")
-      (entry == "ccn.txt" || entry == "pii.txt") && File.size(entry) > 0
-    }
-    if found_pii.empty?
-      return false
-    elsif found_pii.length == 2
-      puts "Found SSN and Credit Card Number"
-    else
-      puts found_pii[0] == "pii.txt" ? "Found SSN" : "Found Credit Card Number"
-    end
-    FileUtils.rm_rf("../output2") # output dir must not exist
-    File.delete(filename)
-    return found_pii
-  end
-
-  def scan_virus
-
-  end
 end
