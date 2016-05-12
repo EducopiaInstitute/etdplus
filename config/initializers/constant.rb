d4d6742e54db644ece49d85268b02d39b24e0f18
Yinlin Chen <ylchen@vt.edu>
2016-05-12 14:02:23 -0400

    Includes XML scan event in export bagit

---
+++ config/initializers/constant.rb
@@ -0,0 +1,8 @@
+# ETDPlus constant strings
+Etdplus::Application.config.x.successful_message = "Successful"
+# PII scan messages
+Etdplus::Application.config.x.pii_pass_message = "Passed PII scan check"
+# Virus scan messages
+Etdplus::Application.config.x.virus_pass_message = "Passed Virus scan check"
+# XML scan messages
+Etdplus::Application.config.x.xml_pass_message = "Passed XML scan check"
