86f87d4b4e00df09a76b67cb711a4904ae0ffdd5
Collin Brittle <cnbrittle@gmail.com>
2016-05-13 19:22:48 +0000

    Avoid crash from certain mime types.

--- app/helpers/etdplus_helper.rb
+++ app/helpers/etdplus_helper.rb
@@ -2,15 +2,19 @@ module EtdplusHelper
 
   def fits_tools_to_display(parent_element)
     blacklist = Rails.configuration.x.fits_display_blacklist
-    if parent_element.name == 'identity'
-      attr_array = fits_identity_attributes(parent_element).map {|a| a.name }
-      parent_element.element_children.reject { |tool| 
-        blacklist.keys.include?(tool["toolname"].to_sym) && blacklist[tool["toolname"].to_sym].any? { |t| (['*', tool.name] + attr_array).include? t}
-      }
+    if parent_element
+      if parent_element.name == 'identity'
+        attr_array = fits_identity_attributes(parent_element).map {|a| a.name }
+        parent_element.element_children.reject { |tool| 
+          blacklist.keys.include?(tool["toolname"].to_sym) && blacklist[tool["toolname"].to_sym].any? { |t| (['*', tool.name] + attr_array).include? t}
+        }
+      else
+        parent_element.element_children.reject { |tool| 
+          blacklist.keys.include?(tool["toolname"].to_sym) && blacklist[tool["toolname"].to_sym].any? { |t| ['*', tool.name].include? t}
+        }
+      end
     else
-      parent_element.element_children.reject { |tool| 
-        blacklist.keys.include?(tool["toolname"].to_sym) && blacklist[tool["toolname"].to_sym].any? { |t| ['*', tool.name].include? t}
-      }
+      []
     end
   end
 
